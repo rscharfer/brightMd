10492bd6c0ecf72e7ea355b17d2ed593d36429f6
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2020-12-02 10:06:16 +0100

    Branding component complete

--- src/App.js
+++ src/App.js
@@ -33,6 +33,38 @@ const DAYS = {
   SATURDAY: "saturday",
 };
 
+function NavBar({
+  mode,
+  title,
+  subtitle,
+  saveHandler,
+  cancelHandler,
+  editHandler,
+}) {
+  return (
+    <div className="headerWrapper">
+      <header>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </header>
+      <div>
+        {mode === "edit" ? (
+          <>
+            <button className="cancelButton" onClick={cancelHandler}>
+              Cancel
+            </button>
+            <button className="saveButton" onClick={saveHandler}>
+              Save
+            </button>
+          </>
+        ) : (
+          <button onClick={editHandler}>Edit</button>
+        )}
+      </div>
+    </div>
+  );
+}
+
 function Day({ dayOfWeek, mode }) {
   const { open, startTime, closeTime } = useHoursOfOperationContext(
     dayOfWeek,
@@ -117,38 +149,115 @@ function App() {
     dispatch({ type: "SAVE" });
     setMode("view");
   }
+
+  const cancelHandler = () => setMode("view");
+  const editHandler = () => setMode("edit");
   return (
     <div className="wrapper">
       <h1>{mode}</h1>
-      <div className="headerWrapper">
-        <header>
-          <h1>Hours of operation</h1>
-          <p>
-            Manage standard hours of operation when providers are able to
+      <NavBar
+        mode={mode}
+        title="Hours of operation"
+        subtitle="Manage standard hours of operation when providers are able to
             provide care. Patients will be informed if they submit an exam
-            outside of these hours.
-          </p>
-        </header>
-        <div>
-          {mode === "edit" ? (
-            <>
-              <button className="cancelButton" onClick={() => setMode("view")}>
-                Cancel
-              </button>
-              <button className="saveButton" onClick={saveHandler}>
-                Save
-              </button>
-            </>
-          ) : (
-            <button onClick={() => setMode("edit")}>Edit</button>
-          )}
-        </div>
-      </div>
+            outside of these hours."
+        saveHandler={saveHandler}
+        cancelHandler={cancelHandler}
+        editHandler={editHandler}
+      />
+
       <section>
         {Object.values(DAYS).map((day) => (
           <Day key={day} dayOfWeek={day} mode={mode} />
         ))}
       </section>
+    </div>
+  );
+}
+
+function Branding(props) {
+  const [editState, setEditState] = React.useState({
+    fullName: "Bright.md Hospital",
+    shortName: "BMD",
+    welcomeText: "Get a quick diagnosis for many madication conditions",
+  });
+
+  const [viewState, setViewState] = React.useState({
+    fullName: "Bright.md Hospital",
+    shortName: "BMD",
+    welcomeText: "Get a quick diagnosis for many madication conditions",
+  });
+
+  const [mode, setMode] = React.useState("edit");
+
+  const editHandler = () => setMode("edit");
+  const saveHandler = () => {
+    setMode("view");
+    setViewState(editState);
+  };
+  const cancelHandler = () => setMode("view");
+
+  function changeHandler(name) {
+    return (e) =>
+      setEditState((oldState) => ({ ...oldState, [name]: e.target.value }));
+  }
+
+  return (
+    <div className="wrapper">
+      <NavBar
+        mode={mode}
+        title="Branding"
+        subtitle="Set name, welcome page text and other branding for your patients to see during an exam"
+        saveHandler={saveHandler}
+        cancelHandler={cancelHandler}
+        editHandler={editHandler}
+      />
+      {mode === "view" ? (
+        <>
+          <div className="brandRow">
+            <div className="inputLabelView">Full Name</div>
+            <div className="inputValueView">{viewState.fullName}</div>
+          </div>
+          <div className="brandRow">
+            <div className="inputLabelView">Short Name</div>
+            <div className="inputValueView">{viewState.shortName}</div>
+          </div>
+          <div className="brandRow">
+            <div className="inputLabelView">Welcome Text</div>
+            <div className="inputValueView">{viewState.welcomeText}</div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="brandRow">
+            <label className="inputLabelView">Full Name</label>
+            <input
+              className="inputValueEditView"
+              name="fullName"
+              value={editState.fullName}
+              onChange={changeHandler("fullName")}
+            />
+          </div>
+          <div className="brandRow">
+            <label className="inputLabelView">Short Name</label>
+            <input
+              className="inputValueEditView"
+              name="shortName"
+              value={editState.shortName}
+              onChange={changeHandler("shortName")}
+            />
+          </div>
+          <div className="brandRow">
+            <label className="inputLabelView">Welcome Text</label>
+            <input
+              className="inputValueEditView"
+              name="welcomeText"
+              value={editState.welcomeText}
+              onChange={changeHandler("welcomeText")}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
@@ -159,6 +268,8 @@ function WrappedApp(props) {
       <HoursOfOperationProvider>
         <App />
       </HoursOfOperationProvider>
+      <hr/>
+      <Branding />
     </>
   );
 }
